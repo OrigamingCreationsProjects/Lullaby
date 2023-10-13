e88cb6669a41dfc6b0a7f18fda3408ed0257afff
Alexvbfpn <campbell12082000@gmail.com>
2023-10-13 17:31:32 +0200

    Health implementada en jugador
    Se ha implementado el script generico Health que controla la cantidad de vida del gameObject que lo contenga y dentro del Player se han declarado e inizializado las variables necesarias para el control de vida, detección de muerte etc con sus correspondientes eventos. Por último, se ha implementado la lógica de 2 nuevos estados para mejorar el comportamiento que en este caso son HurtPlayerState y DiePlayerState

---
+++ Lullaby/Assets/Scripts/Misc/Health.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Lullaby
+{
+    public class Health : MonoBehaviour
+    {
+        public int initial = 100;
+
+        public int max = 100;
+        
+        public float coolDown = 1f;
+        
+        /// <summary>
+        /// Called when the health count changed.
+        /// </summary>
+        public UnityEvent onChange;
+
+        /// <summary>
+        /// Called when it receives damage.
+        /// </summary>
+        public UnityEvent onDamage;
+
+        //MEMBER VARIABLES
+        protected int currentHealth;
+        protected float lastDamageTime;
+
+        public int current
+        {
+            get { return currentHealth; }
+
+            protected set
+            {
+                var last = currentHealth;
+
+                if (value != last)
+                {
+                    currentHealth = Mathf.Clamp(value, 0, max);
+                    onChange?.Invoke();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Returns true if the Health is empty.
+        /// </summary>
+        public virtual bool isEmpty => current == 0;
+
+        /// <summary>
+        /// Returns true if it's still recovering from the last damage.
+        /// </summary>
+        public virtual bool recovering => Time.time < lastDamageTime + coolDown;
+        
+        /// <summary>
+        /// Set the current health to a given amount.
+        /// </summary>
+        /// <param name="amount">The total health you want to set</param>
+        public virtual void Set(int amount) => current = amount;
+
+        /// <summary>
+        /// Increases the amount of health.
+        /// </summary>
+        /// <param name="amount">The amount you want to increase.</param>
+        public virtual void Increase(int amount) => current += amount;
+
+        /// <summary>
+        /// Decreases the amount of health.
+        /// </summary>
+        /// <param name="amount">The amount you want to decrease.</param>
+        public virtual void Damage(int amount)
+        {
+            if (!recovering)
+            {
+                current -= Math.Abs(amount);
+                lastDamageTime = Time.time;
+                onDamage?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Set the current health back to its initial value.
+        /// </summary>
+        public virtual void ResetHealth() => current = initial;
+
+        public virtual void Awake() => current = initial;
+    }
+}
---
+++ Lullaby/Assets/Scripts/Player/States/DiePlayerState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Lullaby.Entities.States
+{
+    public class DiePlayerState: PlayerState
+    {
+        protected override void OnEnter(Player player) { }
+
+        protected override void OnExit(Player player) { }
+
+        public override void OnStep(Player player)
+        {
+            player.ApplyGravity();
+            player.ApplyFriction();
+            player.SnapToGround();
+        }
+
+        public override void OnContact(Player player, Collider other) { }
+    }
+}
