b6e7e4b3faee161a0bd6acf5223fc5d79e93817b
Alexvbfpn <campbell12082000@gmail.com>
2023-10-27 19:46:33 +0200

    Rail prototype agregado
    Se ha creado un rail de referencia  y se ha implementado la lógica de detección de railes.

---
+++ Lullaby/Assets/Scripts/Player/States/RailGrindPlayerState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Lullaby.Entities.States
+{
+    public class RailGrindPlayerState : PlayerState
+    {
+        protected bool backwards;
+        protected float speed;
+        protected float lastDashTime;
+        
+        protected override void OnEnter(Player player)
+        {
+           
+        }
+
+        protected override void OnExit(Player player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnStep(Player player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnContact(Player player, Collider other)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        //protected virtual void Evaluate(Player player ...
+        
+    }
+}
