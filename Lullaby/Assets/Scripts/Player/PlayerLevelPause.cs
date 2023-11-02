e2fb7e7507cff6379270c2175c766d3b15dae6d8
Alexvbfpn <campbell12082000@gmail.com>
2023-11-02 11:01:11 +0100

    Logica de pausa prototipada
    Implementado UIAnimator para la logica de desaparición y aparición desacoplada y encapsulada en una única clase
    Implementado PlayerLevelPause para dar al "personaje del jugador" la capacidad de pausar el juego mediante el input.
    Implementado LevelPauser que sea singleton para la lógica de pausa del nivel y que se pueda llamar desde cualquier lado. Así también implementar la lógica relativa a poder pausarse bajo ciertas condiciones

---
+++ Lullaby/Assets/Scripts/Player/PlayerLevelPause.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Level;
+using UnityEngine;
+
+namespace Lullaby.Entities
+{
+    [RequireComponent(typeof(Player))]
+    [AddComponentMenu("Lullaby/Entities/Player Level Pause")]
+    public class PlayerLevelPause : MonoBehaviour
+    {
+        protected Player _player;
+        protected LevelPauser _pauser;
+
+        protected virtual void Start()
+        {
+            _player = GetComponent<Player>();
+            _pauser = LevelPauser.instance;
+        }
+
+        protected virtual void Update()
+        {
+            if (_player.inputs.GetPauseDown())
+            {
+                Debug.Log("Pause button pressed");
+                var value = _pauser.paused;
+                _pauser.Pause(!value);
+            }
+        }
+    }
+}
---
+++ Lullaby/Assets/Scripts/UI/UIAnimator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Lullaby.UI
+{
+    public class UIAnimator : MonoBehaviour
+    {
+        /// <summary>
+        /// Called when the Show action is invoked.
+        /// </summary>
+        public UnityEvent OnShow;
+
+        /// <summary>
+        /// Called when the Hide action is invoked.
+        /// </summary>
+        public UnityEvent OnHide;
+        
+        public bool hidenOnAwake;
+        // Declaramos los triggers del animator en variables porque el acceso a strings directamente es más lento
+        // y ademas evitamos errores al escribir mal el nombre del trigger 
+        public string normalTrigger = "Normal";
+        public string showTrigger = "Show";
+        public string hideTrigger = "Hide";
+
+        protected Animator _animator;
+        protected Canvas _canvas;
+
+        /// <summary>
+        /// Show the UI element
+        /// </summary>
+        public virtual void Show()
+        {
+            //Aqui lanzariamos el trigger del animator
+            OnShow?.Invoke();
+        }
+
+        /// <summary>
+        /// Hide the UI element
+        /// </summary>
+        public virtual void Hide()
+        {
+            //Aqui lanzariamos el trigger del animator
+            OnHide?.Invoke();
+        }
+        
+        /// <summary>
+        /// Change de active state passing a given value.
+        /// </summary>
+        /// <param name="value">The value you want to pass</param>
+        public virtual void SetActive(bool value) => gameObject.SetActive(value);
+
+
+        protected virtual void Awake()
+        {
+            //_animator = GetComponent<Animator>();
+            _canvas = GetComponent<Canvas>();
+            if (hidenOnAwake)
+            {
+                //Aqui lanzariamos el trigger del animator
+                _canvas.enabled = false;
+            }
+        }
+    }
+}
