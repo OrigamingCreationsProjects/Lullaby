e4888751f4b0877c467e72233fdd77797a08c5b7
Alexvbfpn <campbell12082000@gmail.com>
2023-11-05 17:46:33 +0100

    Implementada KillZone
    Implementado script y área para manejar que no se caiga al vacío infinitamente y se haga respawn.

---
+++ Lullaby/Assets/Scripts/Misc/KillZone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Lullaby.Entities;
+using UnityEngine;
+
+namespace Lullaby
+{
+    [RequireComponent(typeof(Collider))]
+    [AddComponentMenu("Lullaby/Misc/Kill Zone")]
+    public class KillZone : MonoBehaviour
+    {
+        protected Collider _collider;
+
+        protected void Start()
+        {
+            _collider = GetComponent<Collider>();
+            _collider.isTrigger = true;
+        }
+
+        protected void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(GameTags.Player))
+            {
+                if (other.TryGetComponent(out Player player))
+                {
+                    player.playerEvents.OnDeadlyFall?.Invoke();
+                }
+            }
+        }
+    }
+}
