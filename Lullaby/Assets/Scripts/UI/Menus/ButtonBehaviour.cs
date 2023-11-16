1340bc152f7435480bf36f31882a6782042fb538
Alexvbfpn <campbell12082000@gmail.com>
2023-11-16 14:34:04 +0100

    Menús versión 0.6
    Pantalla de controles con navegación entre botones y selección del esquema al hacer click o submit

--- Lullaby/Assets/Scripts/UI/Menus/ButtonBehaviour.cs
+++ Lullaby/Assets/Scripts/UI/Menus/ButtonBehaviour.cs
@@ -6,94 +6,132 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(EventTrigger))]
-public class ButtonBehaviour : MonoBehaviour
+namespace Lullaby.UI.Menus
 {
-    public float scaleOffset = 0.35f;
-    public Vector3 moveOffset = new Vector3(0, 0, 0);
-    public float moveTime = 0.3f;
-    public bool shakePosition = true;
-    public bool movePositon = false;
-    
-    private Button _button;
-    private string _onButtonSound = "OnButtonSound";
-    private string _pressButtonSound = "PressButtonSound";
-    private EventTrigger _eventTrigger;
-    private Vector3 _originalPosition;
-    private Vector3 _originalScale;
-    private Tweener tween;
-    
-    private void Start()
+
+
+    [RequireComponent(typeof(EventTrigger))]
+    public class ButtonBehaviour : MonoBehaviour
     {
-        _originalPosition = transform.position;
-        _originalScale = transform.localScale;
-        _button = GetComponent<Button>();
-        _button.onClick.AddListener(ButtonClicked);
-        _eventTrigger = GetComponent<EventTrigger>();
-        //_eventTrigger.OnPointerClick.AddListener(ClickSuspect);
-        EventTrigger.Entry entry = new EventTrigger.Entry();
-        entry.eventID = EventTriggerType.PointerEnter;
+        public float scaleOffset = 0.35f;
+        public bool shakePosition = true;
 
-        // Agrega una función de delegado para manejar el evento PointerClick
-        entry.callback.AddListener((data) => {OnPointerEnter((PointerEventData)data); });
+        protected Button _button;
+        private string _onButtonSound = "OnButtonSound";
+        private string _pressButtonSound = "PressButtonSound";
+        private EventTrigger _eventTrigger;
+        private Vector3 _originalScale;
+        protected Tweener tween;
 
-        // Agrega la entrada del evento al EventTrigger
-        _eventTrigger.triggers.Add(entry);
-        EventTrigger.Entry entry2 = new EventTrigger.Entry();
-        entry2.eventID = EventTriggerType.PointerExit;
+        protected virtual void Start()
+        {
+            _originalScale = transform.localScale;
+            _button = GetComponent<Button>();
+            if (!_button)
+            {
+                _button = GetComponentInChildren<Button>();
+            }
 
-        // Agrega una función de delegado para manejar el evento PointerClick
-        entry2.callback.AddListener((data) => {OnPointerExit((PointerEventData)data); });
+            _button.onClick.AddListener(ButtonClicked);
 
-        // Agrega la entrada del evento al EventTrigger
-        _eventTrigger.triggers.Add(entry2);
-        
-        EventTrigger.Entry entry3 = new EventTrigger.Entry();
-        entry3.eventID = EventTriggerType.PointerClick;
+            #region -- EVENTOS DE POINTER DEL OBJETO --
+            
+            _eventTrigger = GetComponent<EventTrigger>();
+            
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerEnter;
 
-        // Agrega una función de delegado para manejar el evento PointerClick
-        entry3.callback.AddListener((data) => {OnPointerExit((PointerEventData)data); });
+            // Agrega una función de delegado para manejar el evento PointerClick
+            entry.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
 
-        // Agrega la entrada del evento al EventTrigger
-        _eventTrigger.triggers.Add(entry3);
-        
-        //DOTWEEN
-        tween = transform.DOShakePosition(2f, 5f, 3, 60, false, false, 
-                ShakeRandomnessMode.Harmonic).SetLoops(-1, LoopType.Yoyo);
-        tween.Pause();
+            // Agrega la entrada del evento al EventTrigger
+            _eventTrigger.triggers.Add(entry);
+            EventTrigger.Entry entry2 = new EventTrigger.Entry();
+            entry2.eventID = EventTriggerType.PointerExit;
 
-    }
+            // Agrega una función de delegado para manejar el evento PointerClick
+            entry2.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        // El ratón está sobre el botón
-        Debug.Log("El ratón está sobre el botón.");
-        //SoundManager.Instance.PlayOneShot("OnButtonSound");
-        transform.DOScale(new Vector3(
-                transform.localScale.x + scaleOffset, 
-                transform.localScale.y + scaleOffset, 
+            // Agrega la entrada del evento al EventTrigger
+            _eventTrigger.triggers.Add(entry2);
+
+            EventTrigger.Entry entry3 = new EventTrigger.Entry();
+            entry3.eventID = EventTriggerType.PointerClick;
+
+            // Agrega una función de delegado para manejar el evento PointerClick
+            entry3.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+
+            // Agrega la entrada del evento al EventTrigger
+            _eventTrigger.triggers.Add(entry3);
+
+            #endregion
+            
+            #region -- EVENTOS DE POINTER EN EL BUTTON --
+            
+            EventTrigger buttonTrigger = _button.gameObject.GetComponent<EventTrigger>();
+            if (buttonTrigger == null)
+            {
+                buttonTrigger = _button.gameObject.AddComponent<EventTrigger>();
+            }
+            EventTrigger.Entry entry4 = new EventTrigger.Entry();
+            entry4.eventID = EventTriggerType.Select;
+
+            // Agrega una función de delegado para manejar el evento Select
+            entry4.callback.AddListener((data) => OnPointerEnter(data));
+            
+            buttonTrigger.triggers.Add(entry4);
+            
+            EventTrigger.Entry entry5 = new EventTrigger.Entry();
+            entry5.eventID = EventTriggerType.Deselect;
+
+            // Agrega una función de delegado para manejar el evento Deselect
+            entry5.callback.AddListener((data)=> OnPointerExit(data));
+
+            // Agrega la entrada del evento al EventTrigger
+            buttonTrigger.triggers.Add(entry5);
+            
+            #endregion
+            
+            //DOTWEEN
+            // tween = transform.DOShakePosition(2f, 5f, 3, 60, false, false,
+            //     ShakeRandomnessMode.Harmonic).SetLoops(-1, LoopType.Yoyo);
+            // tween.Pause();
+
+        }
+
+        public virtual void OnPointerEnter(BaseEventData eventData)
+        {
+            // El ratón está sobre el botón
+            Debug.Log("El ratón está sobre el botón.");
+            //SoundManager.Instance.PlayOneShot("OnButtonSound");
+            transform.DOScale(new Vector3(
+                transform.localScale.x + scaleOffset,
+                transform.localScale.y + scaleOffset,
                 transform.localScale.z + scaleOffset), 0.1f).SetEase(Ease.InOutExpo);
 
-        transform.DOMove(_originalPosition + moveOffset, moveTime).SetEase(Ease.InOutExpo);
+            // if (shakePosition)
+            // {
+            //     tween.Play();
+            // }
+        }
 
-        if (shakePosition)
+        public virtual void OnPointerExit(BaseEventData eventData)
         {
-            tween.Play();
+            // El ratón ha salido del botón
+            Debug.Log("El ratón ha salido del botón.");
+            transform.DOScale(_originalScale, 0.1f).SetEase(Ease.InOutExpo);
+
+            if (shakePosition) tween.Pause();
+        }
+
+        public virtual void ButtonClicked()
+        {
+            //SoundManager.Instance.Play("PressButtonSound");
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            OnPointerEnter(eventData);
         }
     }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        // El ratón ha salido del botón
-        Debug.Log("El ratón ha salido del botón.");
-        transform.DOScale(_originalScale, 0.1f).SetEase(Ease.InOutExpo);
-        transform.DOMove(_originalPosition, moveTime).SetEase(Ease.InOutExpo);
-        if(shakePosition) tween.Pause();
-    }
-
-    public void ButtonClicked()
-    {
-        //SoundManager.Instance.Play("PressButtonSound");
-    }
-    
 }
---
+++ Lullaby/Assets/Scripts/UI/Menus/Scheme.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Lullaby.UI.Menus
+{
+    [RequireComponent(typeof(Canvas))]
+    public class Scheme : MonoBehaviour
+    {
+        private Canvas _canvas = null; // Usamos canvas pero podría ser un canvas group si queremos hacer fade in - out etc.
+        private SchemeControlsSystem _schemeControlsSystem = null;
+        private void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+        }
+
+        //Utilizamos esta funcion para que el menuManager se encarge de setear todos los paneles o canvas que tiene y cada
+        //panel sepa cual es su manager
+        public void Setup(SchemeControlsSystem schemeControlsSystem)
+        {
+            _schemeControlsSystem = schemeControlsSystem;
+            //Al terminar de setearlo ocultamos el panel para que todos queden ocultos por default
+            //y no dependa de estar tocando cosas en el inspector
+            Hide();
+        }
+    
+        //Las siguientes funciones se encargan de activar y desactivar el canvas para ocultarlo, pero se podria ejecutar
+        //una funcion que hiciera un fade o una animacion de aparicion/desaparicion, fundamentalmente manejan
+        //como se muestran y ocultan los paneles o su componente canvas
+        public void Show()
+        {
+            _canvas.enabled = true;
+            //gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            _canvas.enabled = false;
+            //gameObject.SetActive(false);
+        }
+    }
+}
