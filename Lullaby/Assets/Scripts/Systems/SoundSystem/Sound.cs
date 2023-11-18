8c1299f117a8d9cd42840dd9d187acb7f505e3e9
Alexvbfpn <campbell12082000@gmail.com>
2023-11-18 22:52:51 +0100

    Ajustes de juego versión 0.5
    Falta solo sensibilidad de ratón y testear mixers en WebGL

---
+++ Lullaby/Assets/Scripts/Systems/SoundSystem/Sound.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Systems.SoundSystem
+{
+    [CreateAssetMenu(fileName = "Sound", menuName = "SoundManager/Sound", order = 0)]
+    public class Sound : ScriptableObject
+    {
+        public AudioClip soundClip;
+        [Range(0f, 1f)] public float volume = 0.5f;
+        [Range(.1f, 3f)] public float pitch = 1f;
+        public bool loop = false;
+        [HideInInspector] public AudioSource audioSource;
+        public AudioMixerGroup mixerGroup;
+        public void Play()
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+
+        public void PlayOneShot()
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        public void PlayDelayed(float delay)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayDelayed(delay);
+            }
+        }
+        public void Stop()
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+        }
+
+        public float GetDuration()
+        {
+            return audioSource.clip.length;
+        }
+    }
+}
---
+++ Lullaby/Assets/Scripts/Systems/SoundSystem/SoundList.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Systems.SoundSystem
+{
+    [CreateAssetMenu(fileName = "SoundList", menuName = "SoundManager/SoundList", order = 1)]
+    public class SoundList : ScriptableObject
+    {
+        public Sound[] sounds;
+    }
+}
