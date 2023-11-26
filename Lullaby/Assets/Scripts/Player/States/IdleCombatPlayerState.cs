faef733ac1adc7915d485d91d370d7172f098c38
Alexvbfpn <campbell12082000@gmail.com>
2023-11-26 17:44:57 +0100

    Ajustes varios de modelos, texturas y organización

---
+++ Lullaby/Assets/Scripts/Player/States/IdleCombatPlayerState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Lullaby.Entities.States
+{
+    public class IdleCombatPlayerState : PlayerState
+    {
+        protected override void OnEnter(Player player) { }
+
+        protected override void OnExit(Player player) { }
+
+        public override void OnStep(Player player) { }
+
+        public override void OnContact(Player player, Collider other) { }
+    }
+}
