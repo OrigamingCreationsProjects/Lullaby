c3dd15a3b994015c9e573818a3913118c4b0a11b
Alexvbfpn <campbell12082000@gmail.com>
2023-12-08 00:17:08 +0100

    Prototipo script FaceChanger

---
+++ Lullaby/Assets/Scripts/Misc/Characters/FaceChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Lullaby
+{
+    [AddComponentMenu("Lullaby/Misc/Face Changer")]
+    public class FaceChanger : MonoBehaviour
+    {
+        public SkinnedMeshRenderer faceRenderer;
+        public Renderer eyesRenderer;
+        
+        
+        public void ChangeFace(int faceIndex)
+        {
+            eyesRenderer.material.SetTextureOffset("_BaseMap", new Vector2(faceIndex * .33f, 0));
+        }
+    }
+}
